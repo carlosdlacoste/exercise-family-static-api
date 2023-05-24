fc73eb0d59eaa68d2a48a4d9bf06384e17d49cf4
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-24 01:38:12 +0000

    Inicializados miembros iniciales de la familia en el arreglo self._members y rellenados metodos de datastructures.py

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,31 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]  
+            },
+
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +45,23 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        self._members.append(member)
+        return self._members
 
-    def delete_member(self, id):
+    def delete_member(self, id): 
         # fill this method and update the return
-        pass
+        index = -1
+        for m in self._members:
+            index += 1
+            if m['id'] == id:
+                self._members.pop(index)
+                return self._members
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for m in self._members:
+            if m['id'] == id:
+                return m
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
