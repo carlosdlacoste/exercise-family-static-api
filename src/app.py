95c875d6eb4ac349126fc36e1d5417b8eeec3787
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-26 19:20:16 +0000

    Corregido endpoint get members para que retorne una lista

--- src/app.py
+++ src/app.py
@@ -30,9 +30,9 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
+    response_body = [{
         "family": members
-    }
+    }]
     return jsonify(response_body), 200
 
 @app.route('/member/<int:member_id>', methods=['GET'])
